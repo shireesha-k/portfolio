311b43af132bd92b7d5bb902dcba2fa8bcd5c97a
katravth shireesha <katravaththrisha@gmail.com>
2025-12-14 23:19:36 +0530

    About Section Updated

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -12,9 +12,10 @@ const TAB_DATA = [
         <li>Node.js</li>
         <li>Express</li>
         <li>PostgreSQL</li>
-        <li>Sequelize</li>
         <li>JavaScript</li>
         <li>React</li>
+        <li>Problem Solving</li>
+        <li>Java</li>
       </ul>
     ),
   },
@@ -23,8 +24,9 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+        <li>Z.p.H.s Raapeta</li>
+        <li>C.v Raman Junior College Wanaparthy</li>
+        <li>Jawahar Lal Nehru Technological University,Hyderabd</li>
       </ul>
     ),
   },
@@ -33,8 +35,10 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+        <li>Full Stack Web development Bootcamp</li>
+        <li>Runner up in Karake Chaos conducted by jntuh(Consolation Prize)</li>
+        <li>Participated in National level Hackathon in CMRCET</li>
+        <li>Created a Chatbot in buildathon event JNTUH(Consolation Prize)</li>
       </ul>
     ),
   },
