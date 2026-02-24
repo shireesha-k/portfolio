bd4c3332eaf0f6c00aca35d6b297dc3db2e2cd80
katravth shireesha <katravaththrisha@gmail.com>
2026-02-25 00:02:23 +0530

    Website logo added

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -63,8 +63,8 @@ const AboutSection = () => {
           <p className="text-base lg:text-lg">
             I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
+            working with JavaScript, React, Node.js, Express, PostgreSQL,
+              HTML, CSS, and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
           </p>
